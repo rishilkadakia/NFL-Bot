4cdcbf17f84ebf9cd8770e94f55dc382563065ff
Rishil Kadakia <rishil.kadakia@gmail.com>
2020-12-26 10:34:18 -0700

    Added !calc function.

--- bot.py
+++ bot.py
@@ -40,6 +40,24 @@ async def ban(ctx, member : discord.Member,*, reason = 'No Reason Provided'):
     await ctx.send(f'{member} has been banned from NFL Discord for {reason}.')
     await member.ban(reason=reason)
 
+# !calc <number> <operator> <number>
+@client.command(aliases=['calculate'])
+async def calc(ctx, *, equation):
+    try:
+        list = equation.split(' ')
+        if list[1] == '+':
+            await ctx.send(f'{list[0]} + {list[2]} = {int(list[0]) + int(list[2])}')
+        elif list[1] == '-':
+            await ctx.send(f'{list[0]} - {list[2]} = {int(list[0]) - int(list[2])}')
+        elif list[1].lower() == 'x' or list[1] == '*':
+            await ctx.send(f'{list[0]} x {list[2]} = {int(list[0]) * int(list[2])}')
+        elif list[1] == '/':
+            await ctx.send(f'{list[0]} / {list[2]} = {int(list[0]) / int(list[2])}')
+        else:
+            await ctx.send(f'Could not understand, incorrect format. Please make sure to perform !calc like this: <number> <operator> <number>.\nEx:\n- !calc 4 x 5\n- !calc 3345 + 123\n- !calc 54/3')
+
+    except:
+        await ctx.send(f'Could not understand, incorrect format. Please make sure to perform !calc like this: <number> <operator> <number>.\nEx:\n- !calc 4 x 5\n- !calc 3345 + 123\n- !calc 54/3')
 # Token
 import os
 client.run(os.getenv('TOKEN'))
