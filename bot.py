bcafc9c9f42015a1f19c14e0496f94171a047e1f
Rishil Kadakia <rishil.kadakia@gmail.com>
2020-12-26 02:59:38 -0700

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+import rules
+from rules import rules
+
+# Bot Prefix
+client = commands.Bot(command_prefix='!')
+
+# On Run
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+# !hello
+@client.command()
+async def hello(ctx):
+    await ctx.send('Hi!')
+
+# !rule <number>
+@client.command()
+async def rule(ctx, *, number):
+    await ctx.send(rules[int(number) - 1])
+
+# !purge <amount>
+@client.command(aliases=['clear'])
+async def purge(ctx, amount):
+    await ctx.channel.purge(limit=int(amount) + 1)
+
+# !kick <user>
+@client.command()
+@commands.has_permissions(kick_members = True)
+async def kick(ctx, member : discord.Member,*, reason = 'No Reason Provided'):
+    await member.send(f'You have been kicked from NFL Discord for {reason}.')
+    await member.kick(reason=reason)
+
+# !ban <user>
+@client.command()
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, member : discord.Member,*, reason = 'No Reason Provided'):
+    await ctx.send(f'{member} has been banned from NFL Discord for {reason}.')
+    await member.ban(reason=reason)
+
+# Token
+client.run('NzkyMTg0NTY0MDM0MzA2MDY4.X-aBXg.sGRrxOu0vqAkIecYsknpq2M4_iQ')
---
+++ rules.py
@@ -0,0 +1,10 @@
+rules = [
+    '**Rule 1 - Common Sense:**\nUse of Common Sense is expected in our server at all times. If you think a behaviour is included in any of the below roles, but wasn\'t explicitly stated, it probably is.\nThings included in common sense clause: Starting Fights, Mentioning / Arguing Bans of Other Members, Graphic / Morbid Images, Berating Mods.',
+    '**Rule 2 - Adult Content:**\nDo not post pornographic or other adult imagery or links to such content. Content on your profile is included.',
+    '**Rule 3 - Inappropriate Language:**\nThe discrimination of users in any form is not allowed. Please do not direct swears, slurs, or other insults towards any member and do not post messages which are intended to provoke argument or start drama.',
+    '**Rule 4 - Advertisements / Promotion:**\nAdvertisement of any Discord server is against our rules, as well as links to unknown YouTube, Twitter, etc pages, as they will be interpreted as self-promotion.',
+    '**Rule 5 - Mentions:**\nDon\'t repeatedly use Discord\'s mention feature to mention the same or multiple users.',
+    '**Rule 6 - Spam:**\nRepeatedly sending messages or sending complete nonsensical messages is not appreciated. Our servers bots automatically handle this rule, and may automute or autoban you for repeat offenses.',
+    '**Rule 7 - Team Channel Raiding:**\nTeam Channel Raiding is the use of another teams channel to "rep" your own team or disparage a team in the channel designated to them and is not allowed.',
+    '**Rule 8 - Off-Topic Conversation:**\nKeep in mind the channel topics when you send messages. The channel topics are viewable in the bar above the text section of the channel, next to the channels name.'
+]
