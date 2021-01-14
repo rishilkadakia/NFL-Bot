b6fc28a7b5abf1524723d523a3eb0719315ecfcb
Rishil Kadakia <rishil.kadakia@gmail.com>
2021-01-14 10:27:14 -0700

    Initial Commit

---
+++ nfl_teams.py
@@ -0,0 +1,11 @@
+nfl_teams = [
+    'Arizona Cardinals', 'Atlanta Falcons', 'Baltimore Ravens', 'Buffalo Bills',
+    'Carolina Panthers', 'Chicago Bears', 'Cincinnati Bengals', 'Cleveland Browns',
+    'Dallas Cowboys', 'Denver Broncos', 'Detroit Lions', 'Green Bay Packers',
+    'Houston Texans', 'Indianapolis Colts', 'Baltimore Colts', 'Jacksonville Jaguars',
+    'Kansas City Chiefs', 'Los Angeles Chargers', 'San Diego Chargers', 'Los Angeles Rams',
+    'St. Louis Rams', 'Miami Dolphins', 'Minnesota Vikings', 'New England Patriots',
+    'New Orleans Saints', 'New York Giants', 'New York Jets', 'Oakland Raiders',
+    'Las Vegas Raiders', 'Philadelphia Eagles', 'Pittsburgh Steelers', 'San Francisco 49ers',
+    'Seattle Seahawks', 'Tampa Bay Buccaneers', 'Tennessee Titans', 'Washington Redskins', 'Washington Football Team'
+]
